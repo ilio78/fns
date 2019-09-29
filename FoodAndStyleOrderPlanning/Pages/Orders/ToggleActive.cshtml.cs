480747e6b2498eb3972055e20dfb5e475e1745cb
giorgos.ilios <giorgos.ilios@gmail.com>
2019-09-29 20:18:49 +0200

    Order toggle active / inactive fixed!

---
+++ FoodAndStyleOrderPlanning/Pages/Orders/ToggleActive.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodAndStyleOrderPlanning.Core;
+using FoodAndStyleOrderPlanning.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodAndStyleOrderPlanning.Pages.Orders
+{
+    public class ToggleActiveModel : PageModel
+    {
+        private IData<Order> data;
+
+        public ToggleActiveModel(IData<Order> recipeData)
+        {
+            this.data = recipeData;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Order order = data.GetById(id);
+
+            if (order == null)
+                return RedirectToPage("./List");
+
+            order.IsActive = !order.IsActive;
+
+            data.Update(order);
+            data.Commit();
+
+            return RedirectToPage("./List");
+        }
+    }
+}
